1d2fbf717844694b47f6d84265521586ba9ee610
Wraithan <xwraithanx@gmail.com>
2012-03-04 04:48:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the github-repo-widget add-on.  It contains:
+
+* A program (lib/main.js).
+* A few tests.
+* Some meager documentation.
---
+++ data/get_repos.js
@@ -0,0 +1,30 @@
+addon.port.on("show", function() {
+    var user = gh.user("wraithan");
+    user.repos(function(data) {
+        data.repositories.forEach(function(element) {
+            var repo = $('<tr class="repo"></tr>');
+            $("#repositories").append(repo);
+
+            repo.append($('<td class="name"></td>').append(element.name));
+            repo.append(
+                $("<td></td>").append($('<a href="' + element.url +'" target="_blank"></a>').append("Code"))
+            );
+            if (element.has_issues) {
+                repo.append(
+                    $("<td></td>").append($('<a href="' + element.url +'/issues" target="_blank"></a>').append("Issues"))
+                );
+            } else {
+                repo.append($("<td></td>"));
+            }
+            if ('homepage' in element && element.homepage) {
+                repo.append(
+                    $("<td></td>").append($('<a href="' + element.homepage +'" target="_blank"></a>').append("Home Page"))
+                );
+            } else {
+                repo.append($("<td></td>"));
+            }
+            console.log(element);
+        });
+    });
+    console.log($("#content"));
+});
