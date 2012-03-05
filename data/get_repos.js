25d3da35eec718cb6f19d5162855ca3bcbe05e05
Wraithan <xwraithanx@gmail.com>
2012-03-04 16:42:53 -0800

    use simple-storage and load only once
    progress on #15

--- data/get_repos.js
+++ data/get_repos.js
@@ -1,47 +1,61 @@
-addon.port.on("show", function() {
-    var user = gh.user("wraithan");
-    user.allRepos(function(data) {
-        data.repositories.sort(function(a, b) {
-            if ('pushed_at' in a && !('pushed_at' in b)) {
-                return 1;
-            } else if ('pushed_at' in b && !('pushed_at' in a)) {
-                return -1;
-            } else if (!('pushed_at' in b) && !('pushed_at' in a)) {
-                return 0;
-            } else if (a.pushed_at < b.pushed_at) {
-                return -1;
-            } else if (b.pushed_at < a.pushed_at) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-        data.repositories.forEach(function(element) {
-            var repo = $('<tr class="repo"></tr>');
-            $("#repositories").append(repo);
+var loaded = false;
 
-            repo.append($('<td class="name"></td>').append(element.name));
-            repo.append(
-                $("<td></td>").append($('<a href="' + element.url +'" target="_blank"></a>').append("Code"))
-            );
-            if (element.has_issues) {
-                repo.append(
-                    $("<td></td>").append($('<a href="' + element.url +'/issues" target="_blank"></a>').append("Issues"))
-                );
-            } else {
-                repo.append($("<td></td>"));
-            }
-            if ('homepage' in element && element.homepage) {
-                var homepage = element.homepage;
-                if (homepage.indexOf('://') == -1) {
-                    homepage = 'http://' + homepage;
-                }
-                repo.append(
-                    $("<td></td>").append($('<a href="' + homepage +'" target="_blank"></a>').append($("<nobr></nobr>").append("Home Page")))
-                );
-            } else {
-                repo.append($("<td></td>"));
-            }
-        });
-    });
+addon.port.on("show", function(storage) {
+    if (!loaded) {
+        if (storage.repositories) {
+            loadReposIntoPanel(storage.repositories);
+        } else {
+            var user = gh.user("wraithan");
+            user.allRepos(function(data) {
+                loadReposIntoPanel(data.repositories);
+                storage.repositories = data.repositories;
+            });
+        }
+        loaded = true;
+    }
 });
+
+function loadReposIntoPanel(repositories) {
+    repositories.sort(function(a, b) {
+        if ('pushed_at' in a && !('pushed_at' in b)) {
+            return -1;
+        } else if ('pushed_at' in b && !('pushed_at' in a)) {
+            return 1;
+        } else if (!('pushed_at' in b) && !('pushed_at' in a)) {
+            return 0;
+        } else if (a.pushed_at > b.pushed_at) {
+            return -1;
+        } else if (b.pushed_at > a.pushed_at) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    repositories.forEach(function(element) {
+        var repo = $('<tr class="repo"></tr>');
+        $("#repositories").append(repo);
+
+        repo.append($('<td class="name"></td>').append(element.name));
+        repo.append(
+            $("<td></td>").append($('<a href="' + element.url +'" target="_blank"></a>').append("Code"))
+        );
+        if (element.has_issues) {
+            repo.append(
+                $("<td></td>").append($('<a href="' + element.url +'/issues" target="_blank"></a>').append("Issues"))
+            );
+        } else {
+            repo.append($("<td></td>"));
+        }
+        if ('homepage' in element && element.homepage) {
+            var homepage = element.homepage;
+            if (homepage.indexOf('://') == -1) {
+                homepage = 'http://' + homepage;
+            }
+            repo.append(
+                $("<td></td>").append($('<a href="' + homepage +'" target="_blank"></a>').append($("<nobr></nobr>").append("Home Page")))
+            );
+        } else {
+            repo.append($("<td></td>"));
+        }
+    });
+}
