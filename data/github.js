f1efd167707b2b9af26a1724914c33193a0388fb
Wraithan <xwraithanx@gmail.com>
2012-03-14 17:55:34 -0700

    makes github.js work outside of being an addon

--- data/github.js
+++ data/github.js
@@ -1,4 +1,12 @@
 var github = {};
+github.log = function(message) {
+    if (typeof(addon) === "undefined") {
+        console.log(message);
+    } else {
+        addon.port.emit('log', message);
+    }
+}
+
 github.url = "https://api.github.com";
 
 github.user = function(user, token) {
@@ -18,13 +26,13 @@ github.getRepos = function(callback) {
     var url = this.url + '/users/' + this.username + '/repos' + this.api_token();
     $.ajax(url, {
         success: function(reply) {
-            addon.port.emit('log', 'success');
+            github.log('success');
             callback(reply);
         },
         error: function(jqXHR, error, errorThrown) {
-            addon.port.emit('log', JSON.stringify(jqXHR));
-            addon.port.emit('log', error);
-            addon.port.emit('log', errorThrown);
+            github.log(JSON.stringify(jqXHR));
+            github.log(error);
+            github.log(errorThrown);
 
         }
     });
