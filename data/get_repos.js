8c4dd0202151bbc68db9a7a3ee4e59418c675e48
Wraithan <xwraithanx@gmail.com>
2012-03-23 09:01:42 -0700

    use jquery templating for creating DOM elements
    fixes #26

--- data/get_repos.js
+++ data/get_repos.js
@@ -1,4 +1,4 @@
-if (typeof(githubRepoWidget) === "undefined") {
+if (typeof(githubRepoWidget) === 'undefined') {
     var githubRepoWidget = {};
 }
 
@@ -7,22 +7,22 @@ githubRepoWidget.tab = 'user';
 
 githubRepoWidget.log = function(message) {
     addon.port.emit('log','get_repos.js: ' + message);
-}
+};
 
 githubRepoWidget.openPrefs = function() {
     addon.port.emit('openPrefs');
-}
+};
 
-addon.port.on("show", function(storage) {
+addon.port.on('show', function(storage) {
     if (!githubRepoWidget.loaded || !githubRepoWidget.isCacheValid(storage)) {
-        $("#repositories").html("");
+        $('#repositories').html('');
         if (storage.repositories && githubRepoWidget.isCacheValid(storage)) {
-            githubRepoWidget.log("From Storage!");
+            githubRepoWidget.log('From Storage!');
             githubRepoWidget.loadReposIntoPanel(storage.repositories, storage);
         } else {
-            githubRepoWidget.log("From GitHub!");
+            githubRepoWidget.log('From GitHub!');
             if (!storage.prefs.githubUsername) {
-                $("#repositories").append("No GitHub Username found. Please enter one.");
+                $('#repositories').append('No GitHub Username found. Please enter one.');
                 githubRepoWidget.loaded = false;
                 return;
             }
@@ -35,7 +35,7 @@ addon.port.on("show", function(storage) {
                     addon.port.emit('loadWatchedRepos');
                 }
             } else {
-                $("#repositories").append("No GitHub API Token found. Please enter one to use these tabs.");
+                $('#repositories').append('No GitHub API Token found. Please enter one to use these tabs.');
             }
         }
         githubRepoWidget.loaded = true;
@@ -45,24 +45,24 @@ addon.port.on("show", function(storage) {
 addon.port.on('loadPane', function(repos, storage) {
     githubRepoWidget.log('loadPane');
     githubRepoWidget.loadReposIntoPanel(repos, storage);
-    addon.port.emit("store", [{"key": "githubUsername",
-                               "value": storage.prefs.githubUsername},
-                              {"key": "repositories",
-                               "value": repos},
-                              {"key": "tab",
-                               "value": githubRepoWidget.tab}]);
+    addon.port.emit('store', [{'key': 'githubUsername',
+                               'value': storage.prefs.githubUsername},
+                              {'key': 'repositories',
+                               'value': repos},
+                              {'key': 'tab',
+                               'value': githubRepoWidget.tab}]);
 
 });
 
-addon.port.on("displayPreferenceChanged", function() {
+addon.port.on('displayPreferenceChanged', function() {
     githubRepoWidget.loaded = false;
 });
 
 githubRepoWidget.refresh = function() {
-    addon.port.emit("store", [{"key": "last_updated_at",
-                               "value": 0}]);
-    addon.port.emit("refresh");
-}
+    addon.port.emit('store', [{'key': 'last_updated_at',
+                               'value': 0}]);
+    addon.port.emit('refresh');
+};
 
 githubRepoWidget.selectActiveTab = function(name) {
     githubRepoWidget.log('Tab change: ' + name);
@@ -70,21 +70,21 @@ githubRepoWidget.selectActiveTab = function(name) {
     $('.' + name).addClass('active');
     githubRepoWidget.tab = name;
     githubRepoWidget.refresh();
-}
+};
 
 githubRepoWidget.isCacheValid = function(storage) {
     var newEnough = ((Date.now() - storage.last_updated_at) < 1000*60*storage.prefs.refreshRate);
     var sameUser = storage.githubUsername == storage.prefs.githubUsername;
     var sameTab = storage.tab == githubRepoWidget.tab;
     return newEnough && sameUser && sameTab;
-}
+};
 
 githubRepoWidget.loadReposIntoPanel = function(data, storage) {
     var repositories = data;
     if (data === null) {
-        $("#repositories").append("No repositories found.");
+        $('#repositories').append('No repositories found.');
         return;
-    } else if ("repositories" in data) {
+    } else if ('repositories' in data) {
         repositories = data.repositories;
     }
     repositories.sort(function(a, b) {
@@ -102,30 +102,36 @@ githubRepoWidget.loadReposIntoPanel = function(data, storage) {
             return 0;
         }
     });
-    var nobr = function(content) {
-        return $('<nobr>' + content + '</nobr>');
-    }
     var link = function(url, name) {
-        return $("<td></td>").append($('<a href="' + url +'" target="_blank"></a>').append(nobr(name)));
-    }
+        return $('<td>').append($('<a>', {
+            'href': url,
+            'target': '_blank',
+            'text': name
+        }));
+    };
     repositories.forEach(function(element) {
-        var repo = $('<tr class="repo"></tr>');
-        $("#repositories").append(repo);
+        var repo = $('<tr>', {
+            'class': 'repo'
+        });
+        $('#repositories').append(repo);
 
-        repo.append($('<td class="name"></td>').append(nobr(element.name)));
+        repo.append($('<td>', {
+            'class': 'name',
+            'text': element.name
+        }));
         repo.append(link(element.url, 'Code'));
         if (storage.prefs.showIssues) {
             if (element.has_issues) {
                 repo.append(link(element.url+'/issues', 'Issues'));
             } else {
-                repo.append($("<td></td>"));
+                repo.append($('<td>'));
             }
         }
         if (storage.prefs.showWiki) {
             if (element.has_wiki) {
                 repo.append(link(element.url+'/wiki', 'Wiki'));
             } else {
-                repo.append($("<td></td>"));
+                repo.append($('<td>'));
             }
         }
         if (storage.prefs.showHomePage) {
@@ -134,12 +140,12 @@ githubRepoWidget.loadReposIntoPanel = function(data, storage) {
                 if (homepage.indexOf('://') == -1) {
                     homepage = 'http://' + homepage;
                 }
-                repo.append(link(homepage, "Home Page"));
+                repo.append(link(homepage, 'Home Page'));
             } else {
-                repo.append($("<td></td>"));
+                repo.append($('<td>'));
             }
         }
     });
-    addon.port.emit("store", [{"key": "last_updated_at",
-                               "value": Date.now()}]);
-}
+    addon.port.emit('store', [{'key': 'last_updated_at',
+                               'value': Date.now()}]);
+};
